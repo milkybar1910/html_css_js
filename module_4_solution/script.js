fe1941f3c9b7e0f5dfb7e0b88ebe3ea89b4add1c
siva <sivashankar1326@gmail.com>
2020-10-09 00:30:13 +0530

    little alteration

--- module_4_solution/script.js
+++ module_4_solution/script.js
@@ -10,22 +10,3 @@
     }
   }
   })();
-  // (function(window){
-  // var speakWord = "Hello";
-  // var helloSpeaker = function (name) {
-  //   console.log(speakWord + " " + name);
-  // }
-  // window.helloSpeaker = helloSpeaker;
-  // })(window);
-  
-
-  // (function(window){
-  // var speakWord = "Good Bye";
-  
-  
-  // var byeSpeaker = function (name) {
-  
-  //   console.log(speakWord + " " + name);
-  // }
-  //   window.byeSpeaker = byeSpeaker;
-  // })(window);
